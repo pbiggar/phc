9b911399832d5b989df1d2fc19bcd3d56c0b4e13
Edsko de Vries <edsko@edsko.net>
2007-04-22 13:44:20 +0000

    This test was testing more than one thing (globals and greater-than), which only confuses things. It is enough that f() and g() have a side effect due to printing.

--- test/subjects/codegen/greater_than.php
+++ test/subjects/codegen/greater_than.php
@@ -1,20 +1,13 @@
 <?php
-
 	// We replace > with <= and switch the paramters, after a pattern in the
 	// zend_parser. However, we need to get the order right.
 
-	$x = 0;
-
-	function f () { echo "F called\n"; global $x; return $x++;}
-	function g () { echo "G called\n"; global $x; return $x++;}
+	function f () { echo "F called\n" }
+	function g () { echo "G called\n" }
 
 	if (f () > g ()) print "F is greater\n";
 	else print "G is greater\n";
 
-
 	if (g () >= f ()) print "G is greater or equal\n";
 	else print "F is greater\n";
-
-	var_dump ($x);
-
 ?>
