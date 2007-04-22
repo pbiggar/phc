db056c9b2cd67c2c0ce62a250e820082e747d4d0
Edsko de Vries <edsko@edsko.net>
2007-04-22 13:44:56 +0000

    whoops, typo

--- test/subjects/codegen/greater_than.php
+++ test/subjects/codegen/greater_than.php
@@ -2,8 +2,8 @@
 	// We replace > with <= and switch the paramters, after a pattern in the
 	// zend_parser. However, we need to get the order right.
 
-	function f () { echo "F called\n" }
-	function g () { echo "G called\n" }
+	function f () { echo "F called\n"; }
+	function g () { echo "G called\n"; }
 
 	if (f () > g ()) print "F is greater\n";
 	else print "G is greater\n";
