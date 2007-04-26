eb7de51729d556b0d632a2fef8c74970eb0b36da
Paul Biggar <paul.biggar@gmail.com>
2007-04-26 22:35:12 +0000

    Fix the bug reported Mike Simons in phc_compile_plugin, and a few other problems it suffered from.
    phc_compile_plugin was not very robust, nor did it work properly. It now checks the filename exists, that libtool exists (and checks a few other locations for libtool, notable the current directory, so using phc_compile_plugin from trunk/ will now work), and wont try to link if the compile fails. I also tweaked some wording and added error messages, as well as some comments.
    
    The test was also quite poor, and remains so, though less so than before. It now checks return values, and actually works under success conditions. It also checks that phc can run the plugin, rather than merely that it compiles. It also works if 'make install' has not been run, although it should use -I from autovars, rather than be hard coded.

--- test/framework/compile_plugin_test.php
+++ test/framework/compile_plugin_test.php
@@ -17,21 +17,31 @@ class CompilePluginTest extends NoSubjectTest
 
 	function run_test ($subject)
 	{
+		global $phc;
 		phc_assert ($subject == "All", "Shouldnt have test subjects passed here");
 		global $phc_compile_plugin;
 
 		$command = "cp plugins/tools/purity_test.cpp plugin.cpp 2>&1";
 		list ($output, $return_value) = complete_exec ($command);
-		if ($output)
+		if ($output || $return_value != 0)
 		{
 			$this->mark_failure ("All", $command, $return_value, $output);
 			return;
 		}
 
 
-		$command = "$phc_compile_plugin plugin.cpp 2>&1 > /dev/null "; # throw away stdout. Compiler errors come in on stdout
+# TODO this -I stuff is very iffy. Use autovars
+		$command = "CFLAGS=\"-I. -Isrc -I src/generated\" $phc_compile_plugin plugin.cpp 2>&1 > /dev/null "; # throw away stdout. Compiler errors come in on stderr
 		list ($output, $return_value) = complete_exec ($command);
-		if ($output) $this->mark_failure ("All", $command, $return_value, $output);
+		if ($output || $return_value != 0)
+		{
+			$this->mark_failure ("All", $command, $return_value, $output);
+			return;
+		}
+
+		$command = "$phc --run plugin.la test/subjects/general/if.php 2>&1 ";
+		list ($output, $return_value) = complete_exec ($command);
+		if ($return_value != 0) $this->mark_failure ("All", $command, $return_value, $output);
 		else $this->mark_success ("All");
 	}
 }
