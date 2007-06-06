e1bc2c1a950f5c5b00f7dfa0ad923f3d8011bd08
Paul Biggar <paul.biggar@gmail.com>
2007-06-06 17:09:25 +0000

    Remove an unnecessary check from Check_lowering, and make check_upperig inherit from check_lowering (allowing for the extra cosntructs)

--- src/codegen/Check_uppering.cpp
+++ src/codegen/Check_uppering.cpp
@@ -16,3 +16,34 @@ void Check_uppering::post_label (AST_label* in)
 {
 	assert (false);
 }
+
+void Check_uppering::post_continue (AST_continue* in)
+{
+	// these are allowed again, since we add them
+}
+
+void Check_uppering::post_switch (AST_switch* in)
+{
+	// these are allowed again, since we add them
+}
+
+void Check_uppering::post_if (AST_if* in)
+{
+	// these are allowed again, since we add them
+}
+
+void Check_uppering::post_break (AST_break* in)
+{
+	if (not in->match (new AST_break (new Token_int (2, NULL))))
+	{
+		phc_internal_error ("The single break should jump two levels", in);
+	}
+}
+
+void Check_uppering::post_while (AST_while* in)
+{
+	if (not in->match (new AST_while (new Token_bool (true, NULL), NULL)))
+	{
+		phc_internal_error ("The single while should be while true", in);
+	}
+}
--- src/codegen/Check_uppering.h
+++ src/codegen/Check_uppering.h
@@ -8,13 +8,22 @@
 #ifndef PHC_CHECK_UPPERING_H
 #define PHC_CHECK_UPPERING_H
 
-#include "AST_visitor.h"
+#include "Check_lowering.h"
 
-class Check_uppering : public AST_visitor
+class Check_uppering : public Check_lowering
 {
 	public:
+
+		// these statements are removed
 		void post_goto (AST_goto* in);
 		void post_label (AST_label* in);
+
+		// these statements are re-added
+		void post_continue (AST_continue* in);
+		void post_switch (AST_switch* in);
+		void post_if (AST_if* in);
+		void post_break (AST_break* in);
+		void post_while (AST_while* in);
 };
 
 #endif // PHC_CHECK_UPPERING_H
