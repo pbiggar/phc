f6cd9b387659b1f71be00b44cc1ffb06ab30c082
Paul Biggar <paul.biggar@gmail.com>
2007-06-07 15:03:38 +0000

    Add a set of special cases to the XML_unparser, to deal with filename and line_number. I'm aware that this should really be fixed, rather than patched over, but this is done, and doing it properly isnt high on my priority list. Ideally, line_numbers should use Integer*s and filenames should never be missing.
    A worry I had was that if files had weird line numbers and such, that a second reparse of the xml would lead to errors. Added this to the tests, but no such problem. The first AST should have line_numbers of zero, and the second should have Integer*s with value of zero, but this doesnt materialize (or .equals allows this??). Left the test in anyway.

--- plugins/tests/xml_roundtrip.cpp
+++ plugins/tests/xml_roundtrip.cpp
@@ -16,12 +16,24 @@ using namespace std;
 static void test_roundtrip (AST_php_script* php_script)
 {
 	#ifdef HAVE_XERCES
-	ostringstream xml_output;
-	XML_unparser xml_unparser(xml_output);
-	php_script->visit(&xml_unparser);
-	AST_php_script* reparsed = parse_ast_xml_buffer(new String(xml_output.str()));
 
-	if(php_script->equals(reparsed))
+	// unparse once
+	ostringstream xml_output1;
+	XML_unparser xml_unparser1 (xml_output1);
+	php_script->visit(&xml_unparser1);
+
+	// reparse
+	AST_php_script* reparsed1 = parse_ast_xml_buffer(new String(xml_output1.str()));
+
+	// unparse once
+	ostringstream xml_output2;
+	XML_unparser xml_unparser2 (xml_output2);
+	reparsed1->visit(&xml_unparser2);
+
+	// reparse
+	AST_php_script* reparsed2 = parse_ast_xml_buffer(new String(xml_output2.str()));
+
+	if(php_script->equals(reparsed1) && php_script->equals(reparsed2))
 	{
 		printf("Success\n");
 	}
@@ -29,8 +41,9 @@ static void test_roundtrip (AST_php_script* php_script)
 	{
 		printf("Failure\n");
 		XML_unparser unparser;
-		php_script->visit(&unparser);
-		reparsed->visit(&unparser);
+		php_script->visit(new XML_unparser ());
+		reparsed1->visit(new XML_unparser ());
+		reparsed2->visit(new XML_unparser ());
 	}
 	#else
 	cout << "XML support not built-in" << endl;
@@ -43,8 +56,3 @@ extern "C" void process_hir (AST_php_script* php_script)
 	test_roundtrip (php_script);
 }
 
-
-extern "C" void process_ast (AST_php_script* php_script)
-{
-	test_roundtrip (php_script);
-}
--- src/process_ast/XML_unparser.cpp
+++ src/process_ast/XML_unparser.cpp
@@ -155,6 +155,14 @@ void XML_unparser::print_attribute(string name, Object* attr)
 		print_indent();
 		os << "</string_list></attr>" << endl;
 	}
+	else if (attr == NULL && name == "phc.filename") // TODO we should fix this, not work around it
+	{
+		os << "<attr key=\"" << name << "\"><string>NULL</string></attr>" << endl;
+	}
+	else if(name == "phc.line_number") // TODO: line numbers are just longs. perhaps they should be wrapped
+	{
+		os << "<attr key=\"" << name << "\"><integer>" << reinterpret_cast<long>(attr) << "</integer></attr>" << endl;
+	}
 	else
 		phc_warning("Don't know how to deal with attribute '%s' of type '%s'", name.c_str(), demangle(attr));	
 }
