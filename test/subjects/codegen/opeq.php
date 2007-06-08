e5b972905f99fe5c2779b6dbabe2c098a677d26d
Edsko de Vries <edsko@edsko.net>
2007-06-08 16:27:28 +0000

    Realized while writing the paper that the way we represent OPEQ operators is wrong. Must modify the grammar.

---
+++ test/subjects/codegen/opeq.php
@@ -0,0 +1,13 @@
+<?php
+// Demonstrates that $x += $y cannot be represented as $x = $x + $y
+	function f()
+	{
+		echo "f is called\n";
+		return 1;
+	}
+
+	$x[1] = 10;
+	$x[f()] += 1;
+
+	var_export($x);
+?>
