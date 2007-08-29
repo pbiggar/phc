e3dd1504200e7d7f75c1f1fdb734b0d1157acb03
Paul Biggar <paul.biggar@gmail.com>
2007-08-29 12:25:49 +0000

    Hand reduce 0002 further.

--- test/subjects/reduced/0002.php
+++ test/subjects/reduced/0002.php
@@ -1,7 +1,9 @@
 <?php
 	// Reduced from test/subjects/codegen/functioncalls_evald_functions.php with phc arguments "--run=plugins/tools/debug_zval.la --dump=plugins/tools/debug_zval.la"
-	$TLE1 = 'function fun_r(&$x) { $x = \'x\'; }';
-	$TSe1 = eval($TLE1);
-	$TSe42 = fun_r($r);
-	$TSe306 = debug_zval_dump($r);
+	function fun_r(&$x) 
+	{ 
+		$x = 'x'; 
+	}
+	fun_r($r);
+	debug_zval_dump($r);
 ?>
