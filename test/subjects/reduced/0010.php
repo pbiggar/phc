21775cec5c6b7eb3ae7709088f4ea1a00ccfb42b
Paul Biggar <paul.biggar@gmail.com>
2007-09-06 19:57:07 +0000

    Change the variable names so that the phc generated names dont clash (we need a better way to handle this).

--- test/subjects/reduced/0010.php
+++ test/subjects/reduced/0010.php
@@ -1,10 +1,10 @@
 <?php
 	
 	// Reduced with command line 'test/framework/reduce test/subjects/codegen/return_by_reference_evald_functions.php'
-	$TLE1 = 'function &g()
+	$b = 'function &g()
 	{
 		return $GLOBALS["a"];
 	}';
-	$TSe1 = eval($TLE1);
+	eval($b);
 	$c = g();
 ?>
