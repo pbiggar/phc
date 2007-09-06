d78e0f4f8e3fc4a338fdc8a099ae7fc5a30d56a8
Paul Biggar <paul.biggar@gmail.com>
2007-09-06 19:58:57 +0000

    I'm tired of having to stop working during tests (which must be 7-8 minutes now, to just run the Compiled tests). This is a first attempt at getting tests running in a separate working directory.

--- test/framework/driver.php
+++ test/framework/driver.php
@@ -23,21 +23,27 @@ require_once ("lib/startup.php");
 require_once ("lib/header.php");
 require_once ("lib/autovars.php");
 
-$log_directory =	"test/logs/".date_string ();
-
 if ($opt_clean)
 {
 	echo "rm -Rf ./test/logs/*\n";
 	`rm -Rf ./test/logs/*`;
+	echo "rm -Rf ./test/working/*\n";
+	`rm -Rf ./test/working/*`;
 	exit (0);
 }
-else
-{
-	mkdir ($log_directory);
-	@unlink ("test/logs/latest");
-	symlink ($log_directory, "test/logs/latest");
-}
 
+
+// setup log dir
+$log_directory =	"test/logs/".date_string ();
+mkdir ($log_directory);
+@unlink ("test/logs/latest");
+symlink ($log_directory, "test/logs/latest");
+
+//setup working dir
+$working_directory =	"test/working/".date_string ();
+mkdir ($working_directory);
+@unlink ("test/working/latest");
+symlink ($working_directory, "test/working/latest");
 
 $phc = get_phc ();
 $php = get_php ();
