d42c24ed324871d54a379dc8cd9f60e98b5ef3eb
Paul Biggar <paul.biggar@gmail.com>
2007-09-06 11:47:44 +0000

    Reduce the time spent in reduce considerably. It doesnt really make sense to try to remove 60 statements starting at index 0, then if that fails, try again from index 1. This comes much closer to proper divide-and-conquer/binary-search, and is similar to Andreas Zeller's approach.
    We also add a comment to the generated file, and print a little more information.

---
+++ plugins/tools/add_comment.cpp
@@ -0,0 +1,32 @@
+/*
+ * phc -- the open source PHP compiler
+ * See doc/license/README.license for licensing information
+ *
+ * After each statement, add a debug_zval_dump call to each variable in the statement.
+ */
+
+#include "process_ast/Pass_manager.h"
+
+extern "C" void load (Pass_manager* pm, Plugin_pass* pass)
+{
+	pm->add_before_named_pass (pass, "ast");
+}
+
+extern "C" void run (AST_node* in, Pass_manager* pm, String* option)
+{
+	String * comment = new String ("// ");
+	comment->append (*option);
+	// Add a comment to the first statement
+	AST_php_script *script = dynamic_cast <AST_php_script*> (in);
+	if (script->statements->size () == 0)
+	{
+		// create a statement for the comment
+		AST_nop* nop = new AST_nop ();
+		nop->get_comments()->push_back (comment);
+	}
+	else
+	{
+		// attach the comment to the first statement
+		script->statements->front ()->get_comments ()->push_back (comment);
+	}
+}
