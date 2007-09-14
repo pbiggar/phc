4e53f7e3720eadde792f0c6b0adb5c13a0638cb8
Paul Biggar <paul.biggar@gmail.com>
2007-09-14 11:58:32 +0000

    New failing test case.

---
+++ test/subjects/reduced/0016.php
@@ -0,0 +1,13 @@
+<?php
+	// Reduced with command line 'test/framework/reduce -v test/subjects/codegen/indexing_non_arrays.php'
+	
+	function write($init)
+	{
+		$TSe20 = var_dump($init[$TLE23]);
+	}
+	$check[] = "a";
+	$PLA0 =& each($check);
+	$init = $check[$PLA0[0]];
+
+	write ($init);
+?>
