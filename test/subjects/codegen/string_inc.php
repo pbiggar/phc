1917a29023e86ba329e9c1e10afd61c0c4906c23
Paul Biggar <paul.biggar@gmail.com>
2007-09-26 12:27:11 +0000

    This test demonstrates that we need to call increment_function, and not add function.

---
+++ test/subjects/codegen/string_inc.php
@@ -0,0 +1,34 @@
+<?php
+
+	$str = "asdasd";
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+
+
+	$str = "0as0dasd0";
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+
+	$str = "10";
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+	$str++;
+	var_dump ($str);
+
+?>
