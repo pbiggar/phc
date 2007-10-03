63e2fd87a9828cf357517886f637e05debd2e1ff
Edsko de Vries <edsko@edsko.net>
2007-10-03 15:25:42 +0000

    Test to make sure that the error level is correctly restored.

---
+++ test/subjects/codegen/ignore_errors2.php
@@ -0,0 +1,8 @@
+<?php
+	$x = file_get_contents("BestaatHeusNiet");
+	var_dump($x);
+	$y = @file_get_contents("BestaatHeusNiet");
+	var_dump($y);
+	$z = file_get_contents("BestaatHeusNiet");
+	var_dump($z);
+?>
