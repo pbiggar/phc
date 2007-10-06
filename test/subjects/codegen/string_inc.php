fb9e35d6a5146d2329a47be767f04f2fa65e715a
Paul Biggar <paul.biggar@gmail.com>
2007-10-06 16:03:45 +0000

    An extended string_inc test I had lying about.

--- test/subjects/codegen/string_inc.php
+++ test/subjects/codegen/string_inc.php
@@ -1,34 +1,45 @@
 <?php
 
+	// and post-op
 	$str = "asdasd";
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
+	var_dump ($str++);
+	var_dump ($str++);
+	var_dump ($str++);
+	var_dump ($str++);
 
 
 	$str = "0as0dasd0";
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
+	var_dump ($str++);
+	var_dump ($str++);
+	var_dump ($str++);
+	var_dump ($str++);
 
 	$str = "10";
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
-	$str++;
-	var_dump ($str);
+	var_dump ($str++);
+	var_dump ($str++);
+	var_dump ($str++);
+	var_dump ($str++);
+
+	// and pre-op
+	$str = "asdasd";
+	var_dump (++$str);
+	var_dump (++$str);
+	var_dump (++$str);
+	var_dump (++$str);
+
+
+	$str = "0as0dasd0";
+	var_dump (++$str);
+	var_dump (++$str);
+	var_dump (++$str);
+	var_dump (++$str);
+
+	$str = "10";
+	var_dump (++$str);
+	var_dump (++$str);
+	var_dump (++$str);
+	var_dump (++$str);
+
+
 
 ?>
