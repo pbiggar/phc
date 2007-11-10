bb8bf3194a7103ed82dc78cc8e42d7e30a9ca4c9
Edsko de Vries <edsko@edsko.net>
2007-11-10 21:12:50 +0000

    $x{-1} is unparsed as $x[-1], but actually, that makes perfect sense: strings don't have negative nidices, so this must be an array index.

---
+++ test/subjects/parsing/curlies.php
@@ -0,0 +1,19 @@
+<?php
+	for($i = 0; $i < 2; $i++) ;
+
+	for($i = 0; $i < 2; $i++)
+		echo $i;
+	
+	for($i = 0; $i < 2; $i++) {}
+
+	for($i = 0; $i < 2; $i++)
+	{
+		echo $i;
+	}
+
+	for($i = 0; $i < 2; $i++)
+	{
+		echo $i;
+		echo "\n";
+	}
+?>
