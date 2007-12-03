1e3e25f1e8911093e2842498eedd62d91eab82c2
Paul Biggar <paul.biggar@gmail.com>
2007-12-03 19:17:50 +0000

    Initial version of the view script, showing basic revision information.

--- test/framework/records/view.php
+++ test/framework/records/view.php
@@ -1,14 +1,45 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<title>phc -- the open source PHP compiler</title>
+		<link rel="stylesheet" type="text/css" href="phc.css">
+	</head>
+	<body>
+		<table>
+			<tr>
+				<td>
+					<table width="100%" cellpadding="5">
+						<tr>
+							<th>Revision</th>
+							<th>Author</th>
+							<th>Passes</th>
+							<th>Fails</th>
+							<th>Skips</th>
+							<th>Timeouts</th>
 <?php
 
 	$DB = new PDO ("sqlite:results/results.db");
+	$query = $DB->query ("
+			SELECT	c.revision, c.author, t.pass, t.fail, t.skip, t.timeout
+			FROM		complete AS c, tests AS t
+			WHERE		t.revision == c.revision AND t.testname == 'Total'
+			ORDER BY c.revision DESC
+			");
+	
+	$completes = $query->fetchAll(PDO::FETCH_ASSOC);
 
-	$revs = $DB->query ("SELECT revision FROM complete ORDER BY revision ASC")->fetchAll(PDO::FETCH_COLUMN);
 
-	var_dump ($revs);
-
-	foreach ($revs as $rev)
+	foreach ($completes as $complete)
 	{
-		print "$revs\n";
+		print "<tr>\n";
+		foreach ($complete as $entry)
+		{
+			print "<td style=\"color: white; background-color: #8a7640;\" >$entry</td>";
+		}
+		print "</tr>\n";
 	}
 
 ?>
+		</table>
+	</body>
+</html>
