7dd6081a6f6632e66e2d7ed97181b8810171474f
Paul Biggar <paul.biggar@gmail.com>
2007-12-04 13:33:23 +0000

    Add relative changes to the test results.

--- test/framework/records/view.php
+++ test/framework/records/view.php
@@ -28,15 +28,56 @@
 	
 	$completes = $query->fetchAll(PDO::FETCH_ASSOC);
 
-
+	# arrange by revision
 	foreach ($completes as $complete)
 	{
+		$rev = (int)$complete["revision"];
+		$revisions[$rev] = $complete;
+	}
+
+	# process data
+	foreach ($revisions as $rev => &$data)
+	{
+		$data["difference"] = add_difference ("pass", $data, $revisions[$rev-1]);
+		add_difference ("fail", $data, $revisions[$rev-1]);
+		add_difference ("skip", $data, $revisions[$rev-1]);
+		add_difference ("timeout", $data, $revisions[$rev-1]);
+	}
+
+
+	# print out rows
+	foreach ($revisions as $rev => $data)
+	{
 		print "<tr>\n";
-		foreach ($complete as $entry)
+
+		# pick a color
+		$color = "white";
+		if ($data["difference"] > 0)
+			$color = "green";
+		elseif ($data["difference"] < 0)
+			$color = "red";
+		unset ($data["difference"]);
+
+
+		foreach ($data as $key => $value)
 		{
-			print "<td style=\"color: white; background-color: #8a7640;\" >$entry</td>";
+			print "<td style=\"color: $color; background-color: #8a7640;\" >$value</td>";
 		}
 		print "</tr>\n";
+	}
+
+	# add a string with the difference of the old data to the new data
+	function add_difference ($name, &$new, $old)
+	{
+		if ($old)
+		{
+			$difference = $new [$name] - $old[$name];
+			if ($difference < 0)
+				$new[$name] .= " ($difference)";
+			elseif ($difference > 0)
+				$new[$name] .= " (+$difference)";
+		}
+		return $difference;
 	}
 
 ?>
