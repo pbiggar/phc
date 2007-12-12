5d2e2d0b7e4cdfb1753102d6c62f2635302ea052
Paul Biggar <paul.biggar@gmail.com>
2007-12-12 17:15:05 +0000

    Add new test, which fails.

---
+++ test/subjects/parsing/scoped_function.php
@@ -0,0 +1,10 @@
+<?php
+
+	x ();
+	// The braces move it out of the list of functions which are read on script
+	// loading. This could be handled by translating to if (true) { ... }
+	{
+		function x () { print "Called\n"; }
+	}
+
+?>
