e3f676e5727c2ff2a945f594cff8bfa7a9cbf409
Paul Biggar <paul.biggar@gmail.com>
2007-12-14 13:43:57 +0000

    Add failed tests to the record front-end.

--- test/framework/records/view.php
+++ test/framework/records/view.php
@@ -17,14 +17,27 @@
 		print "<th>Test Date</th>\n";
 		print "<th>Testing revision</th>\n";
 
+		// successful
 		$query = $DB->query ("
-				SELECT	c.revision, c.author, t.pass, t.fail, t.skip, t.timeout, c.test_date, c.test_revision
+				SELECT	c.revision, c.author, t.pass, t.fail, t.skip, t.timeout, c.test_date, c.test_revision, c.failed
 				FROM		complete AS c, tests AS t
 				WHERE		t.revision == c.revision AND t.testname == 'Total'
 				ORDER BY c.revision DESC
 				");
+		$pass_completes = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
+		// failed
+		$query = $DB->query ("
+				SELECT	revision, author, '' AS pass, '' AS fail, '' as skip, '' as timeout, test_date, test_revision, failed
+				FROM		complete
+				WHERE		failed == 1
+				ORDER BY revision DESC
+				");
+		$fail_completes = $query->fetchAll(PDO::FETCH_ASSOC);
+
+		$completes = array_merge ($pass_completes, $fail_completes);
 		
-		$completes = $query->fetchAll(PDO::FETCH_ASSOC);
 
 		# arrange by revision
 		foreach ($completes as $complete)
@@ -32,6 +45,8 @@
 			$rev = (int)$complete["revision"];
 			$revisions[$rev] = $complete;
 		}
+
+		krsort ($revisions);
 
 		# process data
 		foreach ($revisions as $rev => &$data)
@@ -42,6 +57,7 @@
 			add_difference ("timeout", $data, $revisions[$rev-1]);
 
 			$data["test_date"] = date_from_timestamp ($data["test_date"]);
+			unset ($data["failed"]);
 		}
 
 
