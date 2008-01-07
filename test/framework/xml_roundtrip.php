9c7629f83bfd288938af1d7c0c64e56004531ed9
Paul Biggar <paul.biggar@gmail.com>
2008-01-07 13:51:26 +0000

    Replace the xml_roundtrip with one which reads from a phc instance and writes the the next one.
    We construct a long series of piped command lines, such as
    	phc --read-xml=pass1 --xdump=pass2 | phc -- read-xml=pass2 --xdump=pass3 ...
    
    The final result should be the same as if it were run straight through phc without being dumped to XML. 
    
    In actual fact, we fail in a number of places, which should be fixed. The primary lpace we fail is in names of temporary variables. We should attempt to fix that in the future.

--- test/framework/xml_roundtrip.php
+++ test/framework/xml_roundtrip.php
@@ -6,14 +6,9 @@
  * Check the XML output of a script is the same as if it was parsed from XML
  */
 
-array_push($tests, new XMLRoundtrip());
-class XMLRoundtrip extends PluginTest
+array_push($tests, new XML_roundtrip());
+class XML_roundtrip extends AsyncTest
 {
-	function __construct ()
-	{
-		parent::__construct("xml_roundtrip");
-	}
-
 	function get_dependent_test_names ()
 	{
 		return array ("BasicParseTest");
@@ -26,6 +21,65 @@ class XMLRoundtrip extends PluginTest
 			$xerces_compiled_in != 'no';
 	}
 	
+	function get_test_subjects ()
+	{
+		return get_all_scripts ();
+	}
+
+	// Dump after the last pass. Then dump after every pass, and reparse
+	// it. Check that the final dump is that same.
+	function run_test ($subject)
+	{
+		global $phc;
+		$bundle = new AsyncBundle ($this, $subject);
+
+		$passes = get_pass_list ();
+		$last_pass = $passes [count ($passes) - 1];
+
+		# Get the output going straight though the compiler
+		$bundle->commands[0]		= $this->get_command_line (NULL, $last_pass, $subject);
+
+
+		// Construct a command line which pipes the xml output of one pass into the XML input of the next pass.
+		$long_command = "";
+		$prev_pass = NULL;
+		$concatenator = "";
+		foreach (get_pass_list () as $pass)
+		{
+			$long_command .= $concatenator . $this->get_command_line ($prev_pass, $pass, $subject);
+			
+			$subject = NULL; // we dont need it after the first one
+			$concatenator = " | "; // we want to pipe  the commands together
+			$prev_pass = $pass;
+		}
+
+		$bundle->commands[1] = $long_command;
+
+		$bundle->final = "finish";
+
+		$bundle->start ();
+
+	}
+
+	function get_command_line ($in_pass, $out_pass, $subject)
+	{
+		global $phc;
+		$command = "$phc --no-hash-bang --xdump=$out_pass";
+
+		if (is_string ($subject)) $command .= " $subject";
+		if (is_string ($in_pass)) $command .= " --read-xml=$in_pass";
+
+		return $command;
+	}
+
+	function finish ($bundle)
+	{
+		$expected_final_xml = $bundle->outs[0];
+		$final_xml = $bundle->outs[1];
+		if ($final_xml === $expected_final_xml)
+			$this->mark_success ($bundle->subject);
+		else
+			$this->mark_failure ("Final output doesnt match expected", $bundle);
+	}
+
 }
-
-
