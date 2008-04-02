5f9d01fd9ab1776a3c8f5d136fa1d25c3ceab752
Paul Biggar <paul.biggar@gmail.com>
2008-04-02 17:31:47 +0000

    There are attributes which are lost in the IR. To combat this, we assert that every node must have attributes cloned from its 'creator'. Who its creator is, is not defined.
    But, for example, when this is lowered:
    
    	f (5);
    
    to:
    
    	$T1 = 5;
    	$T2 = f ($T1);
    
    we might say that 5 is the originator of $T1, and f() is the originiator of $T2.
    
    This will also help ensure that filename and line_number attributes are propagated through the compiler.

---
+++ plugins/tests/lost_attributes.cpp
@@ -0,0 +1,68 @@
+/*
+ * phc -- the open source PHP compiler
+ * See doc/license/README.license for licensing information
+ *
+ * Test whether we create nodes without attributes.
+ */
+
+#include "process_ir/General.h"
+#include "pass_manager/Plugin_pass.h"
+#include "AST_visitor.h"
+#include "HIR_visitor.h"
+#include "MIR_visitor.h"
+
+template <class Node, class Visitor>
+class Lost_attributes : public Visitor
+{
+private:
+	bool init;
+
+public:
+	Lost_attributes (bool init = false) : init (init) { }
+
+	void pre_node (Node* in)
+	{
+		if (init)
+		{
+			in->attrs->set_true ("phc.lost_attributes.test");
+		}
+		else if (!in->attrs->is_true ("phc.lost_attributes.test"))
+		{
+			debug (in);
+			xdebug (in);
+			cout << "Failure" << endl;
+			exit (-1);
+		}
+	}
+};
+
+extern "C" void run_ast (AST::PHP_script* in, Pass_manager*, String* option)
+{
+	static bool first = true;
+	if (first)
+	{
+		in->visit (new Lost_attributes<AST::Node, AST::Visitor> (true));
+		first = false;
+	}
+	in->visit (new Lost_attributes<AST::Node, AST::Visitor>);
+}
+
+extern "C" void run_hir (HIR::PHP_script* in, Pass_manager*, String* option)
+{
+	in->visit (new Lost_attributes<HIR::Node, HIR::Visitor>);
+}
+
+extern "C" void run_mir (MIR::PHP_script* in, Pass_manager*, String* option)
+{
+	in->visit (new Lost_attributes<MIR::Node, MIR::Visitor>);
+}
+
+extern "C" void load (Pass_manager* pm, Plugin_pass* pass)
+{
+	pm->add_after_each_pass (pass);
+}
+
+extern "C" void unload ()
+{
+	printf("Success\n");
+}
