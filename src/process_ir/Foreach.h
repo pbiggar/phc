7f9de00b23409d887e93e131d0aef2626b87a76e
Paul Biggar <paul.biggar@gmail.com>
2008-05-16 19:28:50 +0000

    for_lci has served well, however, it wont work with a template type. tfor_lci has been added for this case. This adds a 'typename' keywork to work around areas where the C++ parser is flawed.

--- src/process_ir/Foreach.h
+++ src/process_ir/Foreach.h
@@ -11,13 +11,21 @@
 /* lci stand for List const iterator. VAR is the list, TYPE is the type
  * contained in the list, and ITER is the name of the iterator, which the macro
  * declares for you */
-#define for_lci(VAR, TYPE, ITER) for (List<TYPE*>::const_iterator (ITER) = (VAR)->begin ();	\
-													(ITER) != (VAR)->end ();											\
-													(ITER)++)
+#define for_lci(VAR, TYPE, ITER)										\
+for (List<TYPE*>::const_iterator (ITER) = (VAR)->begin ();	\
+											(ITER) != (VAR)->end ();	\
+											(ITER)++)
 
 // No const.
-#define for_li(VAR, TYPE, ITER) for (List<TYPE*>::iterator (ITER) = (VAR)->begin ();	\
-													(ITER) != (VAR)->end ();											\
-													(ITER)++)
+#define for_li(VAR, TYPE, ITER)										\
+for (List<TYPE*>::iterator (ITER) = (VAR)->begin ();			\
+									(ITER) != (VAR)->end ();			\
+									(ITER)++)
+
+// Templates may need the typename keyworkd
+#define tfor_lci(VAR, TYPE, ITER)												\
+for (typename List<TYPE*>::const_iterator (ITER) = (VAR)->begin ();	\
+														(ITER) != (VAR)->end ();	\
+														(ITER)++)
 
 #endif
