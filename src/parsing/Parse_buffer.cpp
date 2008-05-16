9ed32097e0d2d0f4e05d1c40849675af60452bb3
Paul Biggar <paul.biggar@gmail.com>
2008-05-16 14:18:51 +0000

    Remove code duplication, including trivial code changes for readability.

--- src/parsing/Parse_buffer.cpp
+++ src/parsing/Parse_buffer.cpp
@@ -11,58 +11,57 @@
 #include "process_hir/HIR_unparser.h"
 #include "process_mir/MIR_unparser.h"
 
-#define create_parse_buffer_definition(NS)												\
-/* When given a list of IR statements, use the << operator to parse the	*/		\
-/* given	string into it. When a IR::Node is passed, use the appropriate */		\
-/* unparser to	generate ssource from it. */												\
-																										\
-/* Left-most */																					\
-NS##_parse_buffer::NS##_parse_buffer& operator<<(List<NS::Statement*>& stmts, string in)\
-{																										\
-	NS##_parse_buffer& out = *(new NS##_parse_buffer ());								\
-	out.stmts = &stmts;																			\
-	out.ss << "<?php " << in;																	\
-	return out;																						\
-}																										\
-NS##_parse_buffer::NS##_parse_buffer& operator<<(List<NS::Statement*>& stmts, NS::Node* in)\
-{																										\
-	NS##_parse_buffer& out = *(new NS##_parse_buffer ());								\
-	out.stmts = &stmts;																			\
-	out.ss << "<?php ";																			\
-																										\
-	stringstream ss;																				\
-	in->visit (new NS##_unparser (ss, true));												\
-	out.ss << ss.str ();																			\
-																										\
-	return out;																						\
-}																										\
-																										\
-																										\
-NS##_parse_buffer::NS##_parse_buffer& operator<<(NS##_parse_buffer& out, NS::Node* in)\
-{																										\
-	stringstream ss;																				\
-	in->visit (new NS##_unparser (ss, true));												\
-	out.ss << ss.str ();																			\
-																										\
-	return out;																						\
-}																										\
-																										\
-NS##_parse_buffer::NS##_parse_buffer& operator<<(NS##_parse_buffer& out, string in)\
-{																										\
-	out.ss << in;																					\
-	return out;																						\
-}																										\
-NS##_parse_buffer::NS##_parse_buffer& operator<<(NS##_parse_buffer& out, String* in)\
-{																										\
-	out.ss << in;																					\
-	return out;																						\
-}																										\
-NS##_parse_buffer::NS##_parse_buffer& operator<<(NS##_parse_buffer& out, int in)\
-{																										\
-	out.ss << in;																					\
-	return out;																						\
+/* When given a list of IR statements, use the << operator to parse the 
+ * given string into it. When a IR::Node is passed, use the appropriate
+ * unparser to	generate ssource from it. */
+#define create_parse_buffer_definition(NS)											\
+																									\
+/* Left-most */																				\
+NS##_parse_buffer::NS##_parse_buffer&													\
+operator<<(List<NS::Statement*>& stmts, string in)									\
+{																									\
+	NS##_parse_buffer& out = *(new NS##_parse_buffer ());							\
+	out.stmts = &stmts;																		\
+	out.ss << "<?php " << in;																\
+	return out;																					\
+}																									\
+NS##_parse_buffer::NS##_parse_buffer&													\
+operator<<(List<NS::Statement*>& stmts, NS::Node* in)								\
+{																									\
+	NS##_parse_buffer& out = stmts << "";												\
+	out << in;																					\
+	return out;																					\
+}																									\
+																									\
+																									\
+NS##_parse_buffer::NS##_parse_buffer&													\
+operator<<(NS##_parse_buffer& out, NS::Node* in)									\
+{																									\
+	stringstream ss;																			\
+	in->visit (new NS##_unparser (ss, true));											\
+	out.ss << ss.str ();																		\
+																									\
+	return out;																					\
+}																									\
+																									\
+NS##_parse_buffer::NS##_parse_buffer&													\
+operator<<(NS##_parse_buffer& out, string in)										\
+{																									\
+	out.ss << in;																				\
+	return out;																					\
+}																									\
+NS##_parse_buffer::NS##_parse_buffer&													\
+operator<<(NS##_parse_buffer& out, String* in)										\
+{																									\
+	out.ss << in;																				\
+	return out;																					\
+}																									\
+NS##_parse_buffer::NS##_parse_buffer&													\
+operator<<(NS##_parse_buffer& out, int in)											\
+{																									\
+	out.ss << in;																				\
+	return out;																					\
 }
-
 
 create_parse_buffer_definition(AST);
 create_parse_buffer_definition(HIR);
