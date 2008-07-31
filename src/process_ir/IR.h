e5aaa9486815b678fd456bcbe4c59dd0411635ef
Paul Biggar <paul.biggar@gmail.com>
2008-07-31 16:37:44 +0000

    Move isa<T> and dyc<T> to Object.h, and make them use Objects, not IR::Nodes, to make them more usable generally.

--- src/process_ir/IR.h
+++ src/process_ir/IR.h
@@ -119,24 +119,4 @@ public:
 
 }
 
-
-/* These are copies of ideas from LLVM. All of our uses of casts can be
- * characterized as one of these. */
-template <class T> bool isa(IR::Node* in) 
-{ 
-	return dynamic_cast<T*> (in) != NULL;
-}
-
-template <class T> T* dyc(IR::Node* in)
-{
-	if (in == NULL) return NULL;
-
-	T* result = dynamic_cast<T*> (in); 
-	assert (result != NULL); 
-	return result;
-}
-
-
-
-
 #endif // PHC_IR_H
