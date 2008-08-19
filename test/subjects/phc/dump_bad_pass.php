bd6baf1961ed49fb07157df37f18692c2e28da08
Paul Biggar <paul.biggar@gmail.com>
2008-08-19 13:18:15 +0000

    We check that when the are command line options which use the name of a pass, a pass of that name actually exists. Extend this for --read-xml, make the text the same, and add a test that this works for --dump.

---
+++ test/subjects/phc/dump_bad_pass.php
@@ -0,0 +1,5 @@
+<?php
+	//	options :: out_regex :: err_regex :: exit_code :: no subject
+	// { phc-option: --dump=no_such_pass }
+	// { phc-usage-error: Pass no_such_pass, specified with flag --dump, is not valid }
+?>
--- test/subjects/phc/read_xml_bad_pass.php
+++ test/subjects/phc/read_xml_bad_pass.php
@@ -1,5 +1,5 @@
 <?php
 	//	options :: out_regex :: err_regex :: exit_code :: no subject
 	// { phc-option: --read-xml=no_such_pass }
-	// { phc-usage-error: Specified pass no_such_pass is not valid }
+	// { phc-usage-error: Pass no_such_pass, specified with flag --read-xml, is not valid }
 ?>
