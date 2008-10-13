b6e15441322d65d1d414b9f746c40a7198ba3522
Paul Biggar <paul.biggar@gmail.com>
2008-10-13 17:33:42 +0000

    Fix expected error message in test.

--- test/subjects/errors/isset_of_ref.php
+++ test/subjects/errors/isset_of_ref.php
@@ -1,3 +1,3 @@
 <?php
-  echo isset(&$a); // { phc-error: syntax error, unexpected '&', expecting T_STRING or T_VARIABLE or '$' }
+  echo isset(&$a); // { phc-error: syntax error, unexpected '&', expecting IDENT or VARIABLE or '$' }
 ?>
