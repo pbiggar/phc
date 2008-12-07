773bc0b5a2b9a22fe7d7688b7dbc18f5102c8418
Paul Biggar <paul.biggar@gmail.com>
2008-12-07 12:46:40 +0000

    Support require (TODO: include_once and require_once).

--- runtime/builtin_functions.c
+++ runtime/builtin_functions.c
@@ -150,3 +150,84 @@ phc_builtin_include (zval* arg, zval* result, char* filename TSRMLS_DC)
     compile_file
 */
 }
+
+phc_builtin_require (zval* arg, zval* result, char* filename TSRMLS_DC)
+{
+  // TODO: php_execute_script does some additional work which we need to
+  // emulate. However, we cant call it directly. And why not?
+
+  // TODO: we expect a string, but it could be anything.
+  // TODO: the file might not exist.
+  //
+
+  // In the event that the Zend engine cannot find the file, after checking the
+  // include path, it tries the current directory. It does this only if the
+  // interpreter is executing, and it checks the interpreters opcodes for a
+  // filename (see streams/plain_wrapper.c:1352)
+ 
+  // TODO since PHP does this hack all over the place, do we need to pretend
+  // the entire program is executing?
+  
+  // An alternative is to add the directory to include_path, but its
+  // semantically incorrect (get_included_path() would give the wrong answer),
+  // and error prone (if people overwrite include_path).
+
+  assert (EG(in_execution) == 0);
+  assert (EG(active_op_array) == NULL);
+  assert (filename != NULL);
+
+  // Check we have a string
+  if (Z_TYPE_P (arg) != IS_STRING)
+    {
+      arg = zvp_clone_ex (arg);
+      convert_to_string (arg);
+    }
+
+  // Pretend the interpreter is running
+  EG(in_execution) = 1;
+  zend_op_array ops;
+  ops.filename = filename;
+  EG(active_op_array) = &ops;
+
+  // Open the file
+  zend_file_handle handle;
+  int success = zend_stream_open (Z_STRVAL_P (arg), &handle TSRMLS_CC);
+
+  // Stop pretending
+  EG(in_execution) = 0;
+  EG(active_op_array) = NULL;
+
+  if (success == SUCCESS)
+    {
+      // run it
+      success = zend_execute_scripts (ZEND_REQUIRE TSRMLS_CC, &result, 1, &handle);
+      assert (success == SUCCESS);
+      zend_stream_close (&handle);
+   }
+  else
+    {
+      php_error_docref("function.require" TSRMLS_CC, E_ERROR, "Failed opening required '%s' (include_path='%s')", php_strip_url_passwd(Z_STRVAL_P (arg)), STR_PRINT(PG(include_path)));
+    }
+
+
+ 
+   //TODO 
+  // What happens in zend_vm_def.h, in the include's handler function:
+
+/*  if (is_once_variety)
+    {
+      if (is_absolutepath (arg))
+	failure = !path_ok (arg) || zend_hash_exists (EG(included_files))
+
+      if (failure)
+	; // do nothing
+      else if (file_open(arg) == success)
+	 compile_file
+	 zend_hash_add_empty_element (EG(included_files), arg)
+      else
+	 error
+      }
+  else
+    compile_file
+*/
+}
