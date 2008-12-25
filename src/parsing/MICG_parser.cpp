02c3df1d172196211446964aeadfc492065edef5
Paul Biggar <paul.biggar@gmail.com>
2008-12-25 03:10:05 +0000

    Add actions for each type. Next step is to begin to interpret the file. Rather than do a 1-pass, in-place version, its probably best to make an AST of sorts, and interpret that. Whether I should use maketea to define the AST is up in the air...

--- src/parsing/MICG_parser.cpp
+++ src/parsing/MICG_parser.cpp
@@ -9,13 +9,14 @@
 
 // Uncomment to debug parsing. Useful for finding bugs in the parsed data as
 // well as in the parser.
-#define BOOST_SPIRIT_DEBUG
+//#define BOOST_SPIRIT_DEBUG
 #define BOOST_SPIRIT_DEBUG_PRINT_SOME 80
 
 #include <boost/spirit.hpp>
 #include <boost/lexical_cast.hpp>
 #include <iostream>
 #include "lib/error.h"
+#include "process_ir/debug.h"
 
 using namespace std;
 using namespace boost::spirit;
@@ -46,6 +47,135 @@ struct skip_grammar : public grammar<skip_grammar>
 	};
 };
 
+
+void r_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("r: " << value);
+}
+
+void pattern_name_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("pattern_name: " << value);
+}
+
+void type_name_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("type_name: " << value);
+}
+
+void param_name_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("param_name: " << value);
+}
+
+void signature_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("signature: " << value);
+}
+
+void attr_name_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("attr_name: " << value);
+}
+
+void equals_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("equals: " << value);
+}
+
+void lookup_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("lookup: " << value);
+}
+
+void expr_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("expr: " << value);
+}
+
+void rule_line_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("rule_line: " << value);
+}
+
+void symbol_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("symbol: " << value);
+}
+
+void _template_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("_template: " << value);
+}
+
+void base_expr_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("base_expr: " << value);
+}
+
+void C_code_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("C_code: " << value);
+}
+
+void macro_call_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("macro_call: " << value);
+}
+
+void interpolation_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("interpolation: " << value);
+}
+
+void quoted_string_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("quoted_string: " << value);
+}
+
+void body_a (const char* first, const char* last)
+{
+	// take off the quotes
+	string value = string (first, last);
+	DEBUG ("body_string: " << value);
+}
+
+
+
 /*
  * Limitations:
  *		- Its hard to put {} around the body of a template. Use @@@ instead.
@@ -59,7 +189,7 @@ struct MICG_grammar : public grammar<MICG_grammar>
 	struct definition
 	{
 		rule<ScannerT> r, pattern_name, type_name, param_name, signature,
-			attr_name, equals, lookup, expr, rule_line, not_eol, empty_line, symbol,
+			attr_name, equals, lookup, expr, rule_line, symbol,
 			_template, base_expr, C_code, macro_call, interpolation, quoted_string, body;
 
 		definition (MICG_grammar const& self)
@@ -74,8 +204,6 @@ struct MICG_grammar : public grammar<MICG_grammar>
 			BOOST_SPIRIT_DEBUG_RULE(lookup);
 			BOOST_SPIRIT_DEBUG_RULE(expr);
 			BOOST_SPIRIT_DEBUG_RULE(rule_line);
-			BOOST_SPIRIT_DEBUG_RULE(not_eol);
-			BOOST_SPIRIT_DEBUG_RULE(empty_line);
 			BOOST_SPIRIT_DEBUG_RULE(symbol);
 			BOOST_SPIRIT_DEBUG_RULE(_template);
 			BOOST_SPIRIT_DEBUG_RULE(base_expr);
@@ -87,30 +215,28 @@ struct MICG_grammar : public grammar<MICG_grammar>
 
 
 
-			not_eol = ~ch_p ('\n') & ~ch_p ('\r');
-
 			// A signature line
-			pattern_name = lexeme_d[+(alpha_p | '_')];
-			type_name = lexeme_d[+lower_p];
-			param_name = lexeme_d[+(upper_p | '_')];
-			signature = pattern_name >> '(' >> list_p(type_name >> param_name, ",") >> ')';
+			pattern_name = lexeme_d[+(alpha_p | '_')][&pattern_name_a];
+			type_name = lexeme_d[+lower_p][&type_name_a];
+			param_name = lexeme_d[+(upper_p | '_')][&param_name_a];
+			signature = (pattern_name >> '(' >> list_p(type_name >> param_name, ",") >> ')')[&signature_a];
 
 			// A rule line
-			attr_name = lexeme_d[+(alpha_p | '_')];
-			symbol = lexeme_d[+(alpha_p | '_')];
-			lookup = param_name >> "." >> attr_name;
-			base_expr = lookup | symbol | param_name;
-			expr = base_expr >> !("==" >> base_expr);
+			attr_name = lexeme_d[+(alpha_p | '_')][&attr_name_a];
+			symbol = lexeme_d[+(alpha_p | '_')][&symbol_a];
+			lookup = (param_name >> "." >> attr_name)[&lookup_a];
+			base_expr = (lookup | symbol | param_name)[&base_expr_a];
+			expr = (base_expr >> !("==" >> base_expr))[&expr_a];
 			rule_line = "where" >> expr;
 
 			// Bodies
-			C_code = lexeme_d[(anychar_p - ch_p('@'))];
+			C_code = lexeme_d[anychar_p - ch_p('@')][&C_code_a];
 
-			quoted_string = confix_p ('"', *anychar_p, '"');
-			macro_call = ch_p('\\') >> pattern_name >> '('
-				>> list_p (param_name | quoted_string, ", ") >> ch_p(')') >> ';';
+			quoted_string = confix_p ('"', *anychar_p, '"')[&quoted_string_a];
+			macro_call = (ch_p('\\') >> pattern_name >> '('
+				>> list_p (param_name | quoted_string, ", ") >> ch_p(')') >> ';')[&macro_call_a];
 
-			interpolation = ('$' >> param_name) | ("${" >> lookup >> '}');
+			interpolation = (('$' >> param_name) | ("${" >> lookup >> '}'))[&interpolation_a];
 
 			// A template
 			body = "@@@" >> *(macro_call | interpolation | C_code) >> "@@@";
@@ -141,263 +267,16 @@ MICG_parser::parse (string str)
 
 	parse_info<> pi = boost::spirit::parse (str.c_str(), g >> end_p, skipg);
 
+#ifdef BOOST_SPIRIT_DEBUG
 	cout
 	<< "stop: " << string (pi.stop).substr (0,500) << "\n"
 	<< "hit: " << pi.hit << "\n"
 	<< "full: " << pi.full << "\n"
 	<< "length: " << pi.length << "\n"
 	;
+#endif
 
-	assert (0);
-}
-/*
-void sexp_a (const char* first, const char* last)
-{
-	string sexp = string (first, last);
-	DEBUG ("sexp: " << sexp);
-
-	// Get the argument list
-	int num_children = num_children_stack.top();
-	num_children_stack.pop();
-
-	List<Object*> args;
-
-	for(int i = 0; i < num_children; i++)
-	{
-		args.push_front(node_stack.top());
-		node_stack.pop();
-	}
-
-	// Get the classname
-	String* classname = dyc<String> (node_stack.top());
-	node_stack.pop ();
-
-	// uppercase first lette
-	(*classname)[0] = toupper ((*classname)[0]);
-
-
-	// Now that we have the classname, just pass all the objects on the stack
-	Object* result = LIR::Node_factory::create (classname->c_str (), &args);
-	if (result == NULL)
-		phc_internal_error ("Could not create object '%s', with sexp '%s",
-			classname->c_str (),
-			sexp.c_str());
-
-	assert (result);
-
-	node_stack.push (result);
-	dump_stack ();
-
+	if (!pi.full)
+		phc_internal_error ("Cannot parse template at %s", string (pi.stop).substr (0,100).c_str ());
 }
 
-void classname_a (const char* first, const char* last)
-{
-	string classname = string (first, last);
-	DEBUG ("classname: " << classname);
-
-	// Just push the name, to be available for sexp.
-	node_stack.push (new String (classname));
-	dump_stack ();
-	num_children_stack.top()++;
-	num_children_stack.push(0);
-}
-
-void value_a (const char* first, const char* last)
-{
-	string value = string (first, last);
-	DEBUG ("value: " << value);
-
-	node_stack.push (new String (value));
-	num_children_stack.top()++;
-	dump_stack ();
-}
-
-void string_a (const char* first, const char* last)
-{
-	// take off the quotes
-	string value = string (first+1, last-1);
-	DEBUG ("string: " << value);
-
-	node_stack.push (new String (value));
-	num_children_stack.top()++;
-	dump_stack ();
-}
-
-
-void number_a (const char* first, const char* last)
-{
-	string value = string (first, last);
-	DEBUG ("number: " << value);
-
-	// HACK: uses INT instead of putting INT in the parsed string. The correct
-	// way to do this is to overwrite LIR_factory to explain what to do when we
-	// see an INT.
-	node_stack.push (new LIR::INT (lexical_cast<int> (value)));
-	num_children_stack.top()++;
-	dump_stack ();
-}
-
-void open_list_a (const char* first, const char* last)
-{
-	string value = string (first, last);
-
-	// Find out the type: if omitted, use statement
-	string type;
-	if (value == "[")
-		type = "statement";
-	else
-		type = value.substr (1, value.size());
-
-	// uppercase first letter
-	type[0] = toupper (type[0]);
-
-	// Save as 'Statement_list'
-	type.append ("_list");
-
-	DEBUG ("open list: " << type);
-	type_stack.push (type);
-	num_children_stack.push(0);
-}
-
-void list_a (const char* first, const char* last)
-{
-	string value = string (first, last);
-	DEBUG ("list: " << value);
-
-	// Get the type
-	string type = type_stack.top ();
-	type_stack.pop ();
-
-	// Get the argument list
-	int num_children = num_children_stack.top();
-	num_children_stack.pop();
-
-	List<Object*> args;
-
-	for(int i = 0; i < num_children; i++)
-	{
-		args.push_front(node_stack.top());
-		node_stack.pop();
-	}
-
-	// Now that we have the classname, just pass all the objects on the stack
-	Object* result = LIR::Node_factory::create (type.c_str (), &args);
-	if (result == NULL)
-		phc_internal_error ("Could not create list of type '%s', with sexp '%s",
-			type.c_str (),
-			value.c_str());
-
-
-	node_stack.push (result);
-	dump_stack ();
-	num_children_stack.top()++;
-}
-
-Object*
-parse_lir (String* s)
-{
-	num_children_stack.push(0);
-	DEBUG (*s);
-
-	rule<> r, sexp, classname, param, value, list, string, ws, number;
-	// TODO: add ability to have shortcuts, like *,& or ==
-
-	
-	classname = +(alpha_p | '_'); // An LIR classname
-	ws = *space_p; // optional space
-	value = (alpha_p | '_') >> *(alnum_p | '_'); // a value (really a string, but no "" required)
-	list = (ch_p('[') >> !classname)[&open_list_a] >> ws >> *(sexp >> ws) >> (ws >> (']'))[&list_a]; // list of Objects
-
-	// Any char except ", or, alternatively, the sequence \"
-	string = '"' >> *((anychar_p - '"') | str_p ("\\\"")) >> '"';
-	number = +digit_p;
-
-	// constructor parameters 
-	param = number[&number_a] | value[&value_a] | sexp | list | string[&string_a];
-
-	// definition: a classname and a number of arguments
-	sexp = ('(' >> classname[&classname_a] >> ws >> *(param >> ws) >> ')')[&sexp_a] ;
-
-
-
-   BOOST_SPIRIT_DEBUG_RULE(classname);
-	BOOST_SPIRIT_DEBUG_RULE(ws);
-	BOOST_SPIRIT_DEBUG_RULE(param);
-	BOOST_SPIRIT_DEBUG_RULE(number);
-	BOOST_SPIRIT_DEBUG_RULE(value);
-	BOOST_SPIRIT_DEBUG_RULE(list);
-	BOOST_SPIRIT_DEBUG_RULE(value);
-	BOOST_SPIRIT_DEBUG_RULE(sexp);
-
-	if (!parse (s->c_str (), sexp | list).full)
-		phc_internal_error ("Bad parse of \n%s\n", s->c_str ());
-
-	Object* result = node_stack.top ();
-
-	if (LIR::Node* node = dynamic_cast<LIR::Node*> (result))
-		node->assert_valid ();
-	else if (LIR::Statement_list* stmt_list = dynamic_cast<LIR::Statement_list*> (result))
-	{
-		foreach (LIR::Statement* stmt, *stmt_list)
-			stmt->assert_valid ();
-	}
-
-
-//	assert (node_stack.size () == 1);
-//	assert (num_children_stack.top () == 1);
-	return result;
-}
-
-List<std::pair<string, string> >* result;
-string first;
-string second;
-
-void first_line_a (const char* start, const char* end)
-{
-	first = string (start, end);
-}
-
-void otherlines_a (const char* start, const char* end)
-{
-	second = string (start, end);
-}
-
-void complete_a (const char* start, const char* end)
-{
-	result->push_back (make_pair<string, string> (first, second));
-}
-
-List<std::pair<string, string> >*
-parse_templates (String* s)
-{
-	result = new List<std::pair<string, string> >;
-	// A very simple parser. We match sets of two strings:
-	//		- The first is the name: a single line, starting in the first column,
-	//		and ending in a ':'. It contains no whitespace
-	//		- The second is the value: contains whitespace in the first line, and
-	//		continues until another first line is matched.
-
-
-	rule<> r, first_line, otherline, end_line_p, not_eol, empty_line, comment;
-	not_eol = ~ch_p ('\n') & ~ch_p ('\r');
-
-
-	comment = ch_p('#') >> *not_eol >> eol_p;
-	first_line = (+(alpha_p | ch_p('_')))[&first_line_a] >> ':' >> eol_p;
-	otherline = +blank_p >> +not_eol >> eol_p;
-	empty_line = (*blank_p >> eol_p);
-
-	r =  +(*comment >> (first_line >> (+otherline)[&otherlines_a])[&complete_a] >> *empty_line);
-
-   BOOST_SPIRIT_DEBUG_RULE(first_line);
-	BOOST_SPIRIT_DEBUG_RULE(otherline);
-	BOOST_SPIRIT_DEBUG_RULE(not_eol);
-	BOOST_SPIRIT_DEBUG_RULE(eol_p);
-
-	if (!parse (s->c_str (), r).full)
-		phc_internal_error ("Bad parse of '%s'", s->c_str ());
-
-	return result;
-}
-
-*/
