b80fc6817b1d9135c81d2a853ac54f92f2cd355e
Paul Biggar <paul.biggar@gmail.com>
2009-01-02 19:22:18 +0000

    We need to check if there is a macro named X before we get the first off the list.
    Debugging prints the name of the macro being instatiated. Its useful to know _which_ of that macro is being instantiated, so I've added a line number.

--- src/codegen/MICG_gen.cpp
+++ src/codegen/MICG_gen.cpp
@@ -50,9 +50,6 @@ MICG_gen::get_macro (string name, Object_list* params)
 			return m;
 	}
 
-	if (macros[name].size () == 0)
-		phc_internal_error ("There is no macro named %s", name.c_str ());
-
 	//		string formals = get_formals_as_string (
 	//			macros[macro_name].front()->signature->formal_parameters);
 
@@ -78,6 +75,10 @@ MICG_gen::instantiate (string macro_name, Object_list* params)
 	// there are multiple macros of the same name, they must use the 'list' type
 	// consistently. Then we can just take the first macro, and use it to expand
 	// the parameters, and still get the correct macro for each parameter list.
+
+	if (macros[macro_name].size () == 0)
+		phc_internal_error ("There is no macro named %s", macro_name.c_str ());
+
 	Macro* first_macro = macros[macro_name].front ();
 
 
@@ -85,11 +86,11 @@ MICG_gen::instantiate (string macro_name, Object_list* params)
 	stringstream ss;
 	foreach (Object_list* actual_params, *this->expand_list_params (first_macro, params))
 	{
-		DEBUG ("Entering " << macro_name << *to_string_rep (actual_params));
 		Macro* m = get_macro (macro_name, actual_params);
+		DEBUG ("Entering " << macro_name << ":" << m->get_line_number () << *to_string_rep (actual_params));
 		Symtable* symtable = get_symtable (m, actual_params);
 		ss << instantiate_body (m->body, symtable);
-		DEBUG ("Exiting " << macro_name << *to_string_rep (actual_params));
+		DEBUG ("Exiting " << macro_name << ":" << m->get_line_number () << *to_string_rep (actual_params));
 	}
 	return ss.str ();
 }
