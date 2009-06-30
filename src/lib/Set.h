91e98853dd505b488539a26102cf61ca3f0fd4f5
Paul Biggar <paul.biggar@gmail.com>
2009-06-30 17:00:56 +0000

    A long time ago, it was useful to consider the possability of a set with every possible value in it, which was represented by the bool "full". Its not used now, and not useful, so I'm removing it.

--- src/lib/Set.h
+++ src/lib/Set.h
@@ -24,39 +24,28 @@ template <
 class Set : public std::set<_Tp, _Compare, _Alloc>, virtual public GC_obj
 {
 public:
-	typedef std::set<
-		_Tp,
-		_Compare,
-		_Alloc
-	> parent;
+	typedef std::set<_Tp, _Compare, _Alloc> parent;
+	typedef Set<_Tp, _Compare, _Alloc > this_type;
 
 public:
-	// If true, the SET holds every possible node of that type.
-	bool full;
-
-public:
-	Set () : full (false) {}
+	Set () {}
 	Set (_Compare comparator) 
 	: parent (comparator)
-	, full (false)
 	{
 	}
 
 	Set (_Tp elem1) 
-	: full (false)
 	{
 		insert (elem1);
 	}
 
 	Set (_Tp elem1, _Tp elem2) 
-	: full (false)
 	{
 		insert (elem1);
 		insert (elem2);
 	}
 
 	Set (_Tp elem1, _Tp elem2,  _Tp elem3) 
-	: full (false)
 	{
 		insert (elem1);
 		insert (elem2);
@@ -64,7 +53,6 @@ public:
 	}
 
 	Set (_Tp elem1, _Tp elem2,  _Tp elem3, _Tp elem4) 
-	: full (false)
 	{
 		insert (elem1);
 		insert (elem2);
@@ -82,49 +70,25 @@ public:
 	// versions since the STL includes only out-of-place versions. In-place
 	// versions could be created in some cases, however, possibly with lower
 	// complexity.
-	Set<_Tp, _Compare, _Alloc>* set_union (Set<_Tp, _Compare, _Alloc>* other)
+	this_type* set_union (this_type* other)
 	{
-		Set<_Tp, _Compare, _Alloc>* result = new Set<_Tp, _Compare, _Alloc> (key_comp ());
+		this_type* result = new this_type (key_comp ());
 
-		if (other->full or full)
-			result->full = true;
-		else
-		{
-			std::set_union (
+		std::set_union (
 					this->begin (), this->end(),
 					other->begin (), other->end (),
 					std::insert_iterator<parent> (*result, result->begin ()));
-		}
 
 		return result;
 	}
-	Set<_Tp, _Compare, _Alloc>* set_intersection (Set<_Tp, _Compare, _Alloc>* other)
+	this_type* set_intersection (this_type* other)
 	{
-		Set<_Tp, _Compare, _Alloc>* result = new Set<_Tp, _Compare, _Alloc> (key_comp ());
+		this_type* result = new this_type (key_comp ());
 
-		if (this->full && other->full)
-		{
-			result->full = true;
-		}
-		else if (this->full)
-		{
-			std::copy (
-					this->begin (), this->end(),
-					std::insert_iterator<parent> (*result, result->begin ()));
-		}
-		else if (other->full)
-		{
-			std::copy (
-					other->begin (), other->end(),
-					std::insert_iterator<parent> (*result, result->begin ()));
-		}
-		else
-		{
-			std::set_intersection (
-					this->begin (), this->end(),
-					other->begin (), other->end (),
-					std::insert_iterator<parent> (*result, result->begin ()));
-		}
+		std::set_intersection (
+				this->begin (), this->end(),
+				other->begin (), other->end (),
+				std::insert_iterator<parent> (*result, result->begin ()));
 
 		return result;
 	}
@@ -132,15 +96,7 @@ public:
 
 	Set<_Tp, _Compare, _Alloc>* set_difference (Set<_Tp, _Compare, _Alloc>* other)
 	{
-		Set<_Tp, _Compare, _Alloc>* result = new Set<_Tp, _Compare, _Alloc> (key_comp ());
-
-		if (other->full)
-			return result;
-		else if (this->full)
-		{
-			result->full = true;
-			return result;
-		}
+		this_type* result = new this_type (key_comp ());
 
 		std::set_difference (
 				this->begin (), this->end(),
@@ -150,26 +106,14 @@ public:
 		return result;
 	}	
 public:
-	// Include all possible values
-	void insert_all ()
-	{
-		full = true;
-	}
-
 	bool has (_Tp entry)
 	{
-		return full || (find (entry) != this->end());
+		return find (entry) != this->end ();
 	}
 
-	Set<_Tp, _Compare, _Alloc>* clone ()
+	this_type* clone ()
 	{
-		Set<_Tp, _Compare, _Alloc>* result = new Set<_Tp, _Compare, _Alloc> (key_comp ());
-		if (full)
-		{
-			result->full = true;
-			return result;
-		}
-
+		this_type* result = new this_type (key_comp ());
 		foreach (_Tp entry, *this)
 		{
 			result->insert (phc_clone (entry));
@@ -178,7 +122,7 @@ public:
 		return result;
 	}
 
-	bool equals (Set<_Tp>* other)
+	bool equals (this_type* other)
 	{
 		if (this->size () != other->size ())
 			return false;
@@ -197,9 +141,6 @@ public:
 	}
 
 public:
-	// TODO when using iterators, assert (!full). We cant iterate through the
-	// full set.
-
 
 	List<_Tp>* to_list ()
 	{
