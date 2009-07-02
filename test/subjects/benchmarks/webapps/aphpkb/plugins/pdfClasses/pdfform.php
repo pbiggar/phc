d7f2af98a1243f986d6977df2d66cc4a9ad2f406
Paul Biggar <paul.biggar@gmail.com>
2009-07-02 21:05:48 +0000

    aphpkb is not worth it.

--- test/subjects/benchmarks/webapps/aphpkb/plugins/pdfClasses/pdfform.php
+++
@@ -1,4 +0,0 @@
-<form action="./plugins/pdfClasses/pdfgen.php" method="POST">
-<input type="hidden" name="pdfa" value="<?php echo $a; ?>" />
-<blockquote>Generate a <input type="submit" class="submitLink" value="PDF" /> of this article.</blockquote>
-</form>
--- test/subjects/benchmarks/webapps/aphpkb/textarea.php
+++
@@ -1,4 +0,0 @@
-<?php //textarea.php  -- default textarea
-echo '<textarea name="' . $textareaname . '" style="width:95%; height:300px">' .
- $textareacontent . '</textarea>';
-?>
