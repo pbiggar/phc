695438c1517ed222f2eb8b18ec8cade49bad637b
Jimmy Cleary <jimmehc@gmail.com>
2009-07-07 16:08:29 +0000

    New test with classes.

---
+++ test/subjects/codegen/OO_test.php
@@ -0,0 +1,29 @@
+<?php
+
+	class a 
+	{
+		function b ()
+		{
+			echo "hi\n";
+		}
+	}
+
+	class d 
+	{
+		function e ()
+		{
+			echo "hello\n";
+		}
+	
+		function f ()
+		{
+			echo "hey\n";
+		}
+	
+	}
+
+		
+	$c = new a;
+	$c->b ();
+	var_dump($c);
+?>
