0d47745449e20c0f4715a8aec9ba71f7910450e7
Paul Biggar <paul.biggar@gmail.com>
2009-07-18 10:22:27 +0000

    Fix the dynamic_includes_counter.

--- misc/dynamic_things_counter.php
+++ misc/dynamic_things_counter.php
@@ -10,10 +10,10 @@
 		$files = get_all_files ($dir);
 		foreach ($files as $file)
 		{
-			$stats = get_file_stats ($file);
+			$file_stats = get_file_stats ($file);
 
-			add_stat ($dir, $file, $stats);
-			echo "done: $file\n";
+			add_stat ($dir, $file, $file_stats);
+			echo "$file\n";
 		}
 	}
 
@@ -52,43 +52,38 @@
 			return array ("0", "0", "0");
 
 
-		$result = split (":", `src/phc $file --run plugins/tools/dynamic_things.la 2>/dev/null`);
+		$result = split (":", $result);
 		$result = split (",", $result[1]);
 		return $result;
 	}
 
-	function add_stat ($package, $file, $stats)
+	function add_stat ($package, $file, $file_stats)
 	{
 		global $stats;
-		print "Adding stats for $package, $file\n";
-		var_dump ($stats);
 
-		$stats[$package]["eval"] += $stats[0];
-		$stats[$package]["include"] += $stats[1];
-		$stats[$package]["dynamic_include"] += $stats[2];
+		$stats[$package]["eval"] += $file_stats[0];
+		$stats[$package]["include"] += $file_stats[1];
+		$stats[$package]["dynamic_include"] += $file_stats[2];
 
-		$stats["all"]["eval"] += $stats[0];
-		$stats["all"]["include"] += $stats[1];
-		$stats["all"]["dynamic_include"] += $stats[2];
+		$stats["all"]["eval"] += $file_stats[0];
+		$stats["all"]["include"] += $file_stats[1];
+		$stats["all"]["dynamic_include"] += $file_stats[2];
 	}
 
 	function dump_stats ()
 	{
-		var_dump ($stats);
 		global $stats;
 
 		foreach ($stats as $package => $actual_stats)
 		{
-			var_dump ($package, $actual_stats);
+			print "$package";
 			foreach ($actual_stats as $key => $val)
 			{
-				print "$package: $key = $val\n";
+				print ", $key = $val";
 			}
+			print "\n";
 		}
 	}
-
-
-
 
 
 ?>
