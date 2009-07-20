94f02bde8c4530fd11a89d275bbc3164c42884ea
Jimmy Cleary <jimmehc@gmail.com>
2009-07-20 17:21:54 +0000

    Add comments to download_php.pl.
    edit run_bench.php to run lists of files.
    
    edit misc/comp to include directories of benches when compiling php.

--- downloaded_php/download_php.pl
+++ downloaded_php/download_php.pl
@@ -2,17 +2,20 @@
 
 use WWW::Mechanize
 
+#Set up Meachanize object and proxy
 $mech = new WWW::Mechanize ();
 $mech->proxy (['https', 'http'], 'http://www-proxy.cs.tcd.ie:8080');
 
-$mech->get ('http://sourceforge.net/search/?words=php&type_of_search=soft&pmode=0&limit=100');
+#Go to first page of results for "php" on sourceforge
+$mech->get ('http://sourceforge.net/search/?words=php&type_of_search=soft&pmode=0&limit=100&offset=0');
 
 do
 {
+	# $current_url is the url of the current search results page, update this and find all download links
 	$current_url = $mech->uri ();
-	
 	@links = $mech->find_all_links (text => 'Download Now!');
-
+	
+	# loop through all download links and download where there is a 'direct link'
 	foreach $link (@links)
 	{
 		print ("Navigating to ");
@@ -27,29 +30,28 @@ do
 			print ("Downloading $filename\n");
 			$filename =~ m/(.*)\..*/;
 			$dirname = $1;
-		#	if (! -e "downloaded_php\/downloads\/$dirname")
-		#	{
-				`mkdir downloaded_php\/downloads\/$dirname\/`;
-				$mech->save_content ("downloaded_php\/downloads\/$dirname\/$filename");
-				`tar -C downloaded_php\/downloads\/$dirname\/ -xvf downloaded_php\/downloads\/$dirname\/$filename 2>/dev/null`;
+			`mkdir downloaded_php\/downloads\/$dirname\/`;
+			$mech->save_content ("downloaded_php\/downloads\/$dirname\/$filename");
+			
+			# Attempt to untar, if that fails, attempt to unzip.  Remove file if either op succeeds.
+			`tar -C downloaded_php\/downloads\/$dirname\/ -xvf downloaded_php\/downloads\/$dirname\/$filename`;
+			if (!$?)
+			{
+				`rm downloaded_php\/downloads\/$dirname\/$filename`;
+			}
+			else
+			{
+				`unzip downloaded_php\/downloads\/$dirname\/$filename -ddownloaded_php\/downloads\/$dirname`;
 				if (!$?)
 				{
 					`rm downloaded_php\/downloads\/$dirname\/$filename`;
 				}
-				else
-				{
-					`unzip downloaded_php\/downloads\/$dirname\/$filename -ddownloaded_php\/downloads\/$dirname`;
-					if (!$?)
-					{
-						`rm downloaded_php\/downloads\/$dirname\/$filename`;
-					}
-				}
 			}
-	#	}
+		}
 		$mech->get ($current_url);
 	}
 
-
+	#Follow link to next page
 	$test = $mech->find_link (text_regex => qr/Next ./);
 	if ($test)
 	{
