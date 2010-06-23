28fc9ae632f86d86aaacdbb7d590d4ce1202dc13
Jacob Metcalfe <cubathy@gmail.com>
2010-06-23 19:42:07 +0000

    Add parse support for 'die' statement

--- src/generated/keywords.h
+++ src/generated/keywords.h
@@ -44,12 +44,12 @@
 struct keyword { char* name; int token; };
 #include <string.h>
 
-#define TOTAL_KEYWORDS 68
+#define TOTAL_KEYWORDS 69
 #define MIN_WORD_LENGTH 2
 #define MAX_WORD_LENGTH 12
 #define MIN_HASH_VALUE 3
-#define MAX_HASH_VALUE 174
-/* maximum key range = 172, duplicates = 0 */
+#define MAX_HASH_VALUE 146
+/* maximum key range = 144, duplicates = 0 */
 
 #ifndef GPERF_DOWNCASE
 #define GPERF_DOWNCASE 1
@@ -104,32 +104,32 @@ hash (register const char *str, register unsigned int len)
 {
   static const unsigned char asso_values[] =
     {
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175,  15,  10,  20,  20,   0,
-       40,   5,   0,  10, 175, 175,  30,   0,   0,  85,
-       80,  20,  45,   0,   0,  50,  15,  70,  30,   0,
-      175, 175, 175, 175, 175,   0, 175,  15,  10,  20,
-       20,   0,  40,   5,   0,  10, 175, 175,  30,   0,
-        0,  85,  80,  20,  45,   0,   0,  50,  15,  70,
-       30,   0, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175, 175, 175, 175, 175,
-      175, 175, 175, 175, 175, 175
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147,  15,  85,   5,  10,   0,
+       30,   0,   0,  30, 147, 147,  25,  65,  45,  55,
+        5,   0,  40,   0,   0,  60,   0,  20,   5,   0,
+      147, 147, 147, 147, 147,  15, 147,  15,  85,   5,
+       10,   0,  30,   0,   0,  30, 147, 147,  25,  65,
+       45,  55,   5,   0,  40,   0,   0,  60,   0,  20,
+        5,   0, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147, 147, 147, 147, 147,
+      147, 147, 147, 147, 147, 147
     };
   register int hval = len;
 
@@ -161,182 +161,183 @@ in_word_set (register const char *str, register unsigned int len)
   static const struct keyword wordlist[] =
     {
       {""}, {""}, {""},
-#line 74 "src/generated_src/php.gperf"
+#line 75 "src/generated_src/php.gperf"
       {"try",					K_TRY},
 #line 30 "src/generated_src/php.gperf"
       {"else",					K_ELSE},
       {""},
 #line 31 "src/generated_src/php.gperf"
       {"elseif",				K_ELSEIF},
-#line 41 "src/generated_src/php.gperf"
+#line 42 "src/generated_src/php.gperf"
       {"extends",				K_EXTENDS},
-      {""}, {""},
-#line 65 "src/generated_src/php.gperf"
-      {"__METHOD__",			K___METHOD__},
       {""},
-#line 46 "src/generated_src/php.gperf"
+#line 22 "src/generated_src/php.gperf"
+      {"case",					K_CASE},
+#line 32 "src/generated_src/php.gperf"
+      {"empty",				K_EMPTY},
+      {""},
+#line 28 "src/generated_src/php.gperf"
+      {"do",					K_DO},
+#line 41 "src/generated_src/php.gperf"
+      {"die",					K_DIE},
+      {""},
+#line 76 "src/generated_src/php.gperf"
+      {"catch",				K_CATCH},
+      {""},
+#line 20 "src/generated_src/php.gperf"
+      {"as",					K_AS},
+      {""},
+#line 37 "src/generated_src/php.gperf"
+      {"endswitch",			K_ENDSWITCH},
+      {""},
+#line 58 "src/generated_src/php.gperf"
+      {"static",				K_STATIC},
+      {""},
+#line 73 "src/generated_src/php.gperf"
+      {"abstract",			K_ABSTRACT},
+      {""},
+#line 23 "src/generated_src/php.gperf"
+      {"class",				K_CLASS},
+      {""}, {""},
+#line 14 "src/generated_src/php.gperf"
+      {"and",					K_AND},
+#line 52 "src/generated_src/php.gperf"
+      {"list",					K_LIST},
+#line 33 "src/generated_src/php.gperf"
+      {"enddeclare",			K_ENDDECLARE},
+      {""},
+#line 47 "src/generated_src/php.gperf"
       {"if",					K_IF},
       {""},
 #line 40 "src/generated_src/php.gperf"
       {"exit",					K_EXIT},
-#line 50 "src/generated_src/php.gperf"
-      {"isset",				K_ISSET},
-#line 58 "src/generated_src/php.gperf"
-      {"switch",				K_SWITCH},
-#line 20 "src/generated_src/php.gperf"
-      {"as",					K_AS},
-      {""},
-#line 67 "src/generated_src/php.gperf"
-      {"interface",			K_INTERFACE},
-#line 49 "src/generated_src/php.gperf"
-      {"instanceof",			K_INSTANCEOF},
-#line 57 "src/generated_src/php.gperf"
-      {"static",				K_STATIC},
-#line 28 "src/generated_src/php.gperf"
-      {"do",					K_DO},
-#line 72 "src/generated_src/php.gperf"
-      {"abstract",			K_ABSTRACT},
-#line 22 "src/generated_src/php.gperf"
-      {"case",					K_CASE},
-#line 24 "src/generated_src/php.gperf"
-      {"const",				K_CONST},
-      {""}, {""},
-#line 25 "src/generated_src/php.gperf"
-      {"continue",			K_CONTINUE},
-#line 37 "src/generated_src/php.gperf"
-      {"endswitch",			K_ENDSWITCH},
-#line 21 "src/generated_src/php.gperf"
-      {"break",				K_BREAK},
-      {""}, {""}, {""},
 #line 51 "src/generated_src/php.gperf"
-      {"list",					K_LIST},
-#line 36 "src/generated_src/php.gperf"
-      {"endif",				K_ENDIF},
-      {""}, {""},
-#line 14 "src/generated_src/php.gperf"
-      {"and",					K_AND},
+      {"isset",				K_ISSET},
+#line 59 "src/generated_src/php.gperf"
+      {"switch",				K_SWITCH},
       {""},
-#line 23 "src/generated_src/php.gperf"
-      {"class",				K_CLASS},
-      {""}, {""}, {""}, {""},
-#line 75 "src/generated_src/php.gperf"
-      {"catch",				K_CATCH},
-      {""}, {""},
-#line 18 "src/generated_src/php.gperf"
-      {"__LINE__",			K___LINE__},
+#line 38 "src/generated_src/php.gperf"
+      {"endwhile",			K_ENDWHILE},
+#line 68 "src/generated_src/php.gperf"
+      {"interface",			K_INTERFACE},
+#line 50 "src/generated_src/php.gperf"
+      {"instanceof",			K_INSTANCEOF},
+      {""},
+#line 71 "src/generated_src/php.gperf"
+      {"private",				K_PRIVATE},
+#line 62 "src/generated_src/php.gperf"
+      {"var",					K_VAR},
 #line 39 "src/generated_src/php.gperf"
       {"eval",					K_EVAL},
-#line 33 "src/generated_src/php.gperf"
-      {"enddeclare",			K_ENDDECLARE},
-      {""}, {""},
-#line 60 "src/generated_src/php.gperf"
-      {"use",					K_USE},
-#line 79 "src/generated_src/php.gperf"
-      {"true",					C_TRUE},
-#line 59 "src/generated_src/php.gperf"
-      {"unset",				K_UNSET},
-      {""}, {""},
-#line 17 "src/generated_src/php.gperf"
-      {"__FILE__",			K___FILE__},
-#line 64 "src/generated_src/php.gperf"
-      {"__CLASS__",			K___CLASS__},
-#line 66 "src/generated_src/php.gperf"
-      {"final",				K_FINAL},
-      {""}, {""},
-#line 61 "src/generated_src/php.gperf"
-      {"var",					K_VAR},
-#line 81 "src/generated_src/php.gperf"
-      {"null",					C_NULL},
-      {""},
+#line 36 "src/generated_src/php.gperf"
+      {"endif",				K_ENDIF},
 #line 34 "src/generated_src/php.gperf"
       {"endfor",				K_ENDFOR},
-#line 47 "src/generated_src/php.gperf"
-      {"include",				K_INCLUDE},
-#line 44 "src/generated_src/php.gperf"
-      {"function",			K_FUNCTION},
-      {""},
-#line 35 "src/generated_src/php.gperf"
-      {"endforeach",			K_ENDFOREACH},
-      {""},
-#line 48 "src/generated_src/php.gperf"
-      {"include_once",		K_INCLUDE_ONCE},
-#line 52 "src/generated_src/php.gperf"
-      {"new",					K_NEW},
-      {""},
-#line 80 "src/generated_src/php.gperf"
-      {"false",				C_FALSE},
-      {""},
 #line 26 "src/generated_src/php.gperf"
       {"declare",				K_DECLARE},
 #line 16 "src/generated_src/php.gperf"
       {"xor",					K_XOR},
-#line 77 "src/generated_src/php.gperf"
-      {"cfunction",			K_CFUNCTION},
-#line 19 "src/generated_src/php.gperf"
-      {"array",				K_ARRAY},
       {""},
-#line 27 "src/generated_src/php.gperf"
-      {"default",				K_DEFAULT},
-      {""}, {""},
-#line 32 "src/generated_src/php.gperf"
-      {"empty",				K_EMPTY},
+#line 35 "src/generated_src/php.gperf"
+      {"endforeach",			K_ENDFOREACH},
+      {""}, {""}, {""},
+#line 65 "src/generated_src/php.gperf"
+      {"__CLASS__",			K___CLASS__},
+#line 24 "src/generated_src/php.gperf"
+      {"const",				K_CONST},
       {""},
 #line 15 "src/generated_src/php.gperf"
       {"or",					K_OR},
-#line 42 "src/generated_src/php.gperf"
-      {"for",					K_FOR},
+#line 25 "src/generated_src/php.gperf"
+      {"continue",			K_CONTINUE},
 #line 29 "src/generated_src/php.gperf"
       {"echo",					K_ECHO},
-      {""}, {""},
-#line 43 "src/generated_src/php.gperf"
-      {"foreach",				K_FOREACH},
-      {""}, {""},
+#line 81 "src/generated_src/php.gperf"
+      {"false",				C_FALSE},
+      {""},
+#line 27 "src/generated_src/php.gperf"
+      {"default",				K_DEFAULT},
+#line 61 "src/generated_src/php.gperf"
+      {"use",					K_USE},
+#line 80 "src/generated_src/php.gperf"
+      {"true",					C_TRUE},
+#line 60 "src/generated_src/php.gperf"
+      {"unset",				K_UNSET},
+      {""},
+#line 48 "src/generated_src/php.gperf"
+      {"include",				K_INCLUDE},
 #line 53 "src/generated_src/php.gperf"
+      {"new",					K_NEW},
+#line 72 "src/generated_src/php.gperf"
+      {"protected",			K_PROTECTED},
+#line 69 "src/generated_src/php.gperf"
+      {"implements",			K_IMPLEMENTS},
+      {""},
+#line 49 "src/generated_src/php.gperf"
+      {"include_once",		K_INCLUDE_ONCE},
+#line 43 "src/generated_src/php.gperf"
+      {"for",					K_FOR},
+      {""},
+#line 19 "src/generated_src/php.gperf"
+      {"array",				K_ARRAY},
+      {""},
+#line 44 "src/generated_src/php.gperf"
+      {"foreach",				K_FOREACH},
+#line 18 "src/generated_src/php.gperf"
+      {"__LINE__",			K___LINE__},
+      {""},
+#line 63 "src/generated_src/php.gperf"
+      {"while",				K_WHILE},
+      {""}, {""},
+#line 17 "src/generated_src/php.gperf"
+      {"__FILE__",			K___FILE__},
+      {""},
+#line 54 "src/generated_src/php.gperf"
       {"print",				K_PRINT},
       {""}, {""},
-#line 38 "src/generated_src/php.gperf"
-      {"endwhile",			K_ENDWHILE},
-      {""}, {""},
-#line 56 "src/generated_src/php.gperf"
-      {"return",				K_RETURN},
-#line 63 "src/generated_src/php.gperf"
-      {"__FUNCTION__",		K___FUNCTION__},
-      {""}, {""}, {""},
 #line 45 "src/generated_src/php.gperf"
-      {"global",				K_GLOBAL},
+      {"function",			K_FUNCTION},
+      {""},
+#line 66 "src/generated_src/php.gperf"
+      {"__METHOD__",			K___METHOD__},
+      {""},
+#line 79 "src/generated_src/php.gperf"
+      {"old_function",		K_OLD_FUNCTION},
+      {""}, {""},
+#line 67 "src/generated_src/php.gperf"
+      {"final",				K_FINAL},
       {""}, {""}, {""},
-#line 73 "src/generated_src/php.gperf"
+#line 82 "src/generated_src/php.gperf"
+      {"null",					C_NULL},
+#line 77 "src/generated_src/php.gperf"
+      {"throw",				K_THROW},
+      {""}, {""}, {""}, {""},
+#line 21 "src/generated_src/php.gperf"
+      {"break",				K_BREAK},
+#line 57 "src/generated_src/php.gperf"
+      {"return",				K_RETURN},
+#line 55 "src/generated_src/php.gperf"
+      {"require",				K_REQUIRE},
+      {""}, {""},
+#line 74 "src/generated_src/php.gperf"
       {"clone",				K_CLONE},
       {""},
-#line 70 "src/generated_src/php.gperf"
-      {"private",				K_PRIVATE},
-      {""}, {""},
-#line 62 "src/generated_src/php.gperf"
-      {"while",				K_WHILE},
+#line 56 "src/generated_src/php.gperf"
+      {"require_once",		K_REQUIRE_ONCE},
+      {""}, {""}, {""}, {""},
+#line 64 "src/generated_src/php.gperf"
+      {"__FUNCTION__",		K___FUNCTION__},
       {""},
 #line 78 "src/generated_src/php.gperf"
-      {"old_function",		K_OLD_FUNCTION},
-      {""}, {""}, {""}, {""},
-#line 54 "src/generated_src/php.gperf"
-      {"require",				K_REQUIRE},
-      {""}, {""}, {""},
-#line 69 "src/generated_src/php.gperf"
+      {"cfunction",			K_CFUNCTION},
+      {""},
+#line 70 "src/generated_src/php.gperf"
       {"public",				K_PUBLIC},
-#line 55 "src/generated_src/php.gperf"
-      {"require_once",		K_REQUIRE_ONCE},
-      {""}, {""},
-#line 68 "src/generated_src/php.gperf"
-      {"implements",			K_IMPLEMENTS},
-      {""}, {""}, {""}, {""},
-#line 76 "src/generated_src/php.gperf"
-      {"throw",				K_THROW},
       {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""},
       {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""},
-      {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""},
-      {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""}, {""},
-      {""}, {""},
-#line 71 "src/generated_src/php.gperf"
-      {"protected",			K_PROTECTED}
+      {""}, {""}, {""}, {""}, {""}, {""},
+#line 46 "src/generated_src/php.gperf"
+      {"global",				K_GLOBAL}
     };
 
   if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
