1b8e82a9ed32e4517916774132998d49a3dc2711
Paul Biggar <paul.biggar@gmail.com>
2010-09-30 08:00:22 +0000

    Fix bug0031.php, which was failing due to syntax errors. php -l noted syntax errors too, so I simply fixed them.

--- test/subjects/bugs/bug0031.php
+++ test/subjects/bugs/bug0031.php
@@ -4,9 +4,9 @@
   //die does not work
 
   die;
-  echo "'die' ignored\n"
+  echo "'die' ignored\n";
   die();
-  echo "'die()' ignored\n"
+  echo "'die()' ignored\n";
   die(1);
   echo "'die(1)' ignored\n"
 
