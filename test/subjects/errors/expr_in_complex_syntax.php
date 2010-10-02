aa4794193d8f1be76715a0bb255fe67f3d48f4b5
Paul Biggar <paul.biggar@gmail.com>
2010-10-02 22:24:44 +0000

    Fix expr_in_complex_syntax, where the error message has changed.

--- test/subjects/errors/expr_in_complex_syntax.php
+++ test/subjects/errors/expr_in_complex_syntax.php
@@ -1,5 +1,5 @@
 <?php
 	$x = 1;
 	$y = 2;
-	echo "foo {$x + $y} bar\n"; // { phc-error: Variable name expected }
+	echo "foo {$x + $y} bar\n"; // { phc-error: Variable name or method invocation expected }
 ?>
