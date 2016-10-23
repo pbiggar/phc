136b66860feb46c340a758e3bf7d641c0df2a97e
Paul Biggar <paul.biggar@gmail.com>
2016-10-23 01:30:07 -0400

    Recent versions of boost broke the foreach macro. Workaround

--- src/process_ir/Foreach.h
+++ src/process_ir/Foreach.h
@@ -9,6 +9,15 @@
 #define PHC_FOREACH
 
 #include <boost/foreach.hpp>
+#include <boost/version.hpp>
+
+namespace boost {
+
+#if BOOST_VERSION != 104900
+  namespace BOOST_FOREACH = foreach;
+#endif
+
+} // namespace boost
 
 #define foreach BOOST_FOREACH
 
