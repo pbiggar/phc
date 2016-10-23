f4a035f1b480f62d70dee2630cfb1667ae0948a1
Paul Biggar <paul.biggar@gmail.com>
2016-10-23 03:55:04 -0400

    Use warnings with autotools

--- autogen.sh
+++ autogen.sh
@@ -4,8 +4,8 @@ set -exou pipefail
 # autotools invocations are hard to get right.
 
 aclocal -I m4 -I libltdl
-autoconf
-automake
+autoconf --warnings=all
+automake --warnings=all
 
 set +x
 echo "To build phc, run \"./configure\" followed by \"make\""
